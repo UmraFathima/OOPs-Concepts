2486fc9cff946fb8cccadeaf9918f50fbf62264f
Umra Fathima <154687173+UmraFathima@users.noreply.github.com>
2025-06-18 19:47:57 +0530

    pass by refernece

---
+++ C&O_6.py
@@ -0,0 +1,15 @@
+class Atm():
+    def __init__(self):
+        self.__balance=0
+        self.__pin="12"
+    def get_Pin(self):
+        return self.__pin
+    def set_Pin(self,newpin):
+        if  type(newpin)==str:
+            self.__pin=newpin
+            print(self.__pin)
+# print(self.__pin)
+
+sbi=Atm()
+sbi.get_Pin()
+sbi.set_Pin("1234")
