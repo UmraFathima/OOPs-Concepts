80a4f3fd205b7b90c7616e51c5a16d232aacac96
Umra Fathima <154687173+UmraFathima@users.noreply.github.com>
2025-06-23 23:32:47 +0530

    example3

--- inheritance.py
+++ inheritance.py
@@ -194,3 +194,28 @@ yo.show()
 yoo=B()
 yoo.show()
         
+ ########################************multilevel inheritance*************############
+
+class Product:
+    def review(self):
+        print("Product customer review")
+
+class Phone(Product):
+    def __init__(self, price, brand, camera):
+        print("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print("Buying a phone")
+
+class SmartPhone(Phone):
+    pass
+
+s = SmartPhone(20000, "Apple", 12)
+p = Phone(1000, "Samsung", 1)
+
+s.buy()
+s.review()
+p.review()
